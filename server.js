67e5db60a5a31976feaded6a73b951cbc4f301e8
enochwu <enochwu7@gmail.com>
2018-02-06 19:41:32 -0800

    Revised request methods
    Correctly requesting things.

--- server.js
+++ server.js
@@ -8,13 +8,19 @@ var PORT2 = 7500
 function handleRequest(request, response) {
 
   // Send the below string to the client when the user visits the PORT URL
-  response.end("It Works!! Path Hit: " + request.url);
+  response.end(PORT1 + ' says: ' + 'YOU\'RE AMAZING!');
+}
+
+function handleRequest2(request, response) {
+
+  // Send the below string to the client when the user visits the PORT URL
+  response.end(PORT2 + ' says: ' + 'Ya got something in your teeth.');
 }
 
 // Use the Node HTTP package to create our server.
 // Pass the handleRequest function to empower it with functionality.
 var server = http.createServer(handleRequest);
-var server2 = http.createServer(handleRequest);
+var server2 = http.createServer(handleRequest2);
 
 // Start our server so that it can begin listening to client requests.
 server.listen(PORT1, function() {
@@ -26,5 +32,5 @@ server.listen(PORT1, function() {
 server2.listen(PORT2, function() {
 
   // Log (server-side) when our server has started
-  console.log(PORT2 + ' says: ' + 'Ya got something in your teethA.');
+  console.log(PORT2 + ' says: ' + 'Ya got something in your teeth.');
 });
